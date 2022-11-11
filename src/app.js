1db43bc2fa825274453d73474048a398e6484300
Jarvis142 <misschristenivey@gmail.com>
2022-11-11 14:07:43 -0500

    Forecast working
    arduous process of figuring out how to get forecast to operate

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
-function formatDate(timestamp){
-    let days = [
+function formatDate(timestamp) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let day = days[date.getDay()];
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hour}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date= new Date(timestamp * 1000)
+  let day= date.getDay()
+let days=[
   "Sunday",
   "Monday",
   "Tuesday",
@@ -7,57 +32,46 @@ function formatDate(timestamp){
   "Thursday",
   "Friday",
   "Saturday"
-];
-    let date= new Date(timestamp);
-    let hour= date.getHours();
-    let minutes = date.getMinutes();
-    let day= days[date.getDay()];
-       if (hour < 10) {
-  hour = `0${hour}`};
-  if (minutes < 10) {
-  minutes = `0${minutes}`}
-    return `${day} ${hour}:${minutes}`
+]
+return days[day];
 }
 function displayForecast(response) {
- console.log(response);
+ console.log(response.data);
+ let forecast= response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days=[
-    "Saturday",
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday"
-  ]
+ 
 
   let forecastHTML = `<div>`;
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6){
+      let maxFah= Math.round(forecastDay.temperature.maximum);
     forecastHTML =
       forecastHTML +
       `
     <div class="week">
+
 <div class="card">
          <div class="card-body">
            <div class="row">
-             <div class="col-3">
- <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" height="35" class="imgIcon" id="icon" />
-           </div>
-            
-            <div class="col-5 weather-forecast-date">${day}</div>
-             <div class="col-2 farenheight">
-               95°F
+             <div class="col-3"> 
+             <img src= "https://ssl.gstatic.com/onebox/weather/64/sunny.png" height="35" class="imgIcon" id="icon" />
+</div>
+           <div class="col-5 weather-forecast-date">${formatDay(forecastDay.time)}</div>
+
+             <div class="col-2 farenheight"> ${Math.round(forecastDay.temperature.maximum * 9) / 5 + 32}
+               °F
              </div>
-             <div class="col-2 celcius">
-               35°C
+             <div class="col-2 celcius">${Math.round(forecastDay.temperature.minimum* 9) / 5 + 32}
+               °F
              </div>
            </div>
          </div>
          </div>
-         
+         </div>
 `;
-  });
+ } });
  
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -72,7 +86,7 @@ function displayTemperature(response) {
     let currentConditions = document.querySelector("#conditions");
     let weatherIcon = document.querySelector("#icon");
     
-    celsiusTemperature = Math.round(response.data.temperature.current);
+    fahrenheitTemperature = Math.round(response.data.temperature.current * 9) /5 + 32;
     
     let feelsLike = document.querySelector("#feelsLike");
     let humidityElement = document.querySelector("#humidity");
@@ -86,19 +100,20 @@ function displayTemperature(response) {
       `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
     weatherIcon.setAttribute("alt", response.data.description);
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
     feelsLike.innerHTML = Math.round(response.data.temperature.feels_like);
     humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
     windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.time * 1000);
-    }
+    
 
-
+getForecast(response.data.coordinates);}
+function getForecast(coordinates){
 let apiKey= "5cdf0fabb9e3fo934af85f67fb2t19a3"; //for some reason when I add {apiKey} to my url it returns an "Invalid" response
 let city= "";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=5cdf0fabb9e3fo934af85f67fb2t19a3&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
-
+let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lat=${coordinates.latitude}&lon=${coordinates.longitude}&key=5cdf0fabb9e3fo934af85f67fb2t19a3&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
 function search(city){
     let apiKey= "5cdf0fabb9e3fo934af85f67fb2t19a3"; //for some reason when I add {apiKey} to my url it returns an "Invalid" response
     let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=5cdf0fabb9e3fo934af85f67fb2t19a3&units=metric`;
@@ -149,4 +164,4 @@ function search(city){
       let celsiusLink = document.querySelector("#celsius-link");;
       celsiusLink.addEventListener("click", convertToCelsius);
 
-      displayForecast();
+ 
